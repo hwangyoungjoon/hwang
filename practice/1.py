0e2f0d5169484640c46996d9dc4cbcf873ad2004
mghysoka <mghysoka@gmail.com>
2020-08-13 19:43:45 +0900

    algorithms test practice

--- practice/1.py
+++ practice/1.py
@@ -1,18 +1,19 @@
-def solution(board, moves):
-    bucket = []
-    answer = []
-    for move in moves:
-        for i in range(len(board)):
-            if board[i][move-1] > 0:
-                bucket.append(board[i][move-1])
-                board[i][move-1] = 0
-                if bucket[-1:] == bucket[-2:-1]:
-                    answer += bucket[-1:]
-                    bucket = bucket[:-2]
-                break
-    return len(answer)*2
-            
-    return answer
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves =	[1,5,3,5,1,2,1,4]
-print(solution(board,moves))
+def solution(tickets):
+    tic_dic = dict()
+    for s,a in tickets:
+        tic_dic[s] = tic_dic.get(s,[]) + [a]
+    for k in tic_dic.keys():
+        tic_dic[k].sort(reverse=True)
+    
+    stack = ["ICN"]
+    visit = []
+    while len(stack) > 0:
+        top = stack[-1]
+        if len(tic_dic.get(top,[])) == 0: ##this node will be not visited
+            visit.append(stack.pop())
+        else:
+            stack.append(tic_dic[top].pop())
+
+    return visit[::-1]
+
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	))
